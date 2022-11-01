c994d36ee6e3aa1ec7e3be116fa5750b8820464a
LIZXP <Peter.xli@outlook.com>
2022-11-01 12:14:34 -0600

    create first test and delete unrelated pending test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    # validation tests/examples here
+    it "check if id is present" do
+      @category = Category.new(name: "testCategory")
+      @product = Product.new(name: "testProduct", price: "100", quantity: "20", category: @category)
+      @product.save
+      expect(@product.id).to be_present
+    end
   end
 end
