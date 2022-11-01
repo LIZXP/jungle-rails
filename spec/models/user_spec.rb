5e47505aece8aac8ce4802ab2e67698c2cd761e8
LIZXP <Peter.xli@outlook.com>
2022-11-01 15:23:54 -0600

    validate for user creation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   describe 'Validations' do
-    # validation examples here
+    it "It must be created with a password and password_confirmation fields" do
+      @user = User.new(name: "Peter", email: "test@test.com", password: "111111", password_confirmation: "111111")
+      @user.save!
+      expect(@user).to be_present
+    end
   end
 
   describe '.authenticate_with_credentials' do
