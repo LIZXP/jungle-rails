5d337c65203ebbdea178b2c4a6af40c8c2a6056f
LIZXP <Peter.xli@outlook.com>
2022-11-01 14:22:33 -0600

    create validation for price and quantity

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -8,5 +8,17 @@ RSpec.describe Product, type: :model do
       @product.save
       expect(@product.id).to be_present
     end
+    it "validate quantity" do
+      @category = Category.new(name: "testCategory")
+      @product = Product.new(name: "testProduct", price: "100", quantity: nil, category: @category)
+      @product.save
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+    it "validate price" do
+      @category = Category.new(name: "testCategory")
+      @product = Product.new(name: "testProduct", quantity: "20", category: @category)
+      @product.save
+      expect(@product.errors.full_messages).to include("Price is not a number")
+    end
   end
 end
